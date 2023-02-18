089c6e31860dea5a46a838b07983994690db8d53
Renee Mumbi <1723renee@gmail.com>
2023-02-18 16:30:01 +0300

    Done

---
+++ lib/magazine.rb
@@ -0,0 +1,13 @@
+class Magazine
+    attr_accessor :name, :category
+    @@all = []
+
+    def initialize(name)
+        @name=name
+        @category=category
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
