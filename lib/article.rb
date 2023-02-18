616f145eac24758d569b780154ac238358c08005
Renee Mumbi <1723renee@gmail.com>
2023-02-18 16:37:46 +0300

    Create Article.all

--- lib/article.rb
+++ lib/article.rb
@@ -1,8 +1,13 @@
 class Article
     attr_accessor :author, :magazine, :title
+    @@ll = []
     def initialize (author, magazine, title)
         @author= author
         @magazine=magazine
         @title=title
+        @@all << self 
     end 
+    def self.all
+        @@all
+    end
 end 
